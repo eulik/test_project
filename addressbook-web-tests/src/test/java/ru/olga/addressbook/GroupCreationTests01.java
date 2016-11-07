17fa8cc643fb83306b3fa27b6b2fbf65e352321e
eulik <veitholga@gmail.com>
2016-11-07 14:07:11 +0100

    refactor

--- addressbook-web-tests/src/test/java/ru/olga/addressbook/GroupCreationTests01.java
+++ addressbook-web-tests/src/test/java/ru/olga/addressbook/GroupCreationTests01.java
@@ -5,16 +5,11 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
-import static org.testng.Assert.*;
 
 import java.util.concurrent.TimeUnit;
-import java.util.Date;
-import java.io.File;
-import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.interactions.Actions;
+
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.*;
-import static org.openqa.selenium.OutputType.*;
 
 public class GroupCreationTests01 {
     FirefoxDriver wd;
@@ -25,36 +20,61 @@ public class GroupCreationTests01 {
         wd = new FirefoxDriver();
         wd.manage().timeouts().implicitlyWait(1000, TimeUnit.MILLISECONDS);
         wd.get("http://localhost/addressbook/");
-        wd.findElement(By.name("pass")).click();
-        wd.findElement(By.name("pass")).sendKeys("secret");
-        wd.findElement(By.name("user")).click();
-        wd.findElement(By.name("user")).clear();
-        wd.findElement(By.name("user")).sendKeys("admin");
-        wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+        login("secret", "admin");
 
     }
-    
+
+    private void login(String password, String username) {
+        wd.findElement(By.name("pass")).click();
+        wd.findElement(By.name("pass")).sendKeys(password);
+        wd.findElement(By.name("user")).click();
+        wd.findElement(By.name("user")).clear();
+        wd.findElement(By.name("user")).sendKeys(username);
+        wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+    }
+
     @Test
     public void group_creation_tests01() {
 
-        //WebElement xxx = wait.until(presenceOfElementLocated(By.linkText("group page")))
-        new WebDriverWait(wd, 10).until(ExpectedConditions.elementToBeClickable(By.linkText("groups")));
-        wd.findElement(By.linkText("groups")).click();
+
+        gotoGroupPage();
+        initGroupCreation();
+        fillGroupForm(new GroupData("group", "test", "test 2"));
+        submitGroupCreation();
+        returnToGroupPage();
+    }
+
+    private void returnToGroupPage() {
+        wd.findElement(By.linkText("group page")).click();
+    }
+
+    private void submitGroupCreation() {
+        wd.findElement(By.name("submit")).click();
+    }
+
+    private void initGroupCreation() {
         wd.findElement(By.name("new")).click();
+    }
+     private void fillGroupForm(GroupData groupData){
+        new WebDriverWait(wd, 10).until(ExpectedConditions.elementToBeClickable(By.name("group_name")));
         wd.findElement(By.name("group_name")).click();
         wd.findElement(By.name("group_name")).clear();
-        wd.findElement(By.name("group_name")).sendKeys("group");
+        wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
         wd.findElement(By.name("group_header")).click();
         wd.findElement(By.name("group_header")).clear();
-        wd.findElement(By.name("group_header")).sendKeys("test");
+        wd.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
         wd.findElement(By.xpath("//div[@id='content']/form")).click();
         wd.findElement(By.name("group_footer")).click();
         wd.findElement(By.name("group_footer")).clear();
-        wd.findElement(By.name("group_footer")).sendKeys("test 2");
-        wd.findElement(By.name("submit")).click();
-        wd.findElement(By.linkText("group page")).click();
+        wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
     }
-    
+
+    private void gotoGroupPage() {
+        //WebElement xxx = wait.until(presenceOfElementLocated(By.linkText("group page")))
+        new WebDriverWait(wd, 10).until(ExpectedConditions.elementToBeClickable(By.linkText("groups")));
+        wd.findElement(By.linkText("groups")).click();
+    }
+
     @AfterMethod
     public void tearDown() {
         wd.quit();
